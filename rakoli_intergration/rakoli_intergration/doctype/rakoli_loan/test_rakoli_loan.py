2c8279dd2b5bf623b7afc9e690c8a66fed9b76bc
av-dev2 <emgani@aakvatech.com>
2026-09-09 17:04:07 +0300

    test: migrate Rakoli Loan tests to IntegrationTestCase

--- rakoli_intergration/rakoli_intergration/doctype/rakoli_loan/test_rakoli_loan.py
+++ rakoli_intergration/rakoli_intergration/doctype/rakoli_loan/test_rakoli_loan.py
@@ -1,5 +1,35 @@
-from frappe.tests.utils import FrappeTestCase
+import frappe
+from frappe.tests import IntegrationTestCase
+
+from rakoli_intergration.tests.utils import create_employee
 
 
-class TestRakoliLoan(FrappeTestCase):
-	pass
+class TestRakoliLoan(IntegrationTestCase):
+	def test_employee_and_loan_amount_are_mandatory(self):
+		with self.assertRaises(frappe.MandatoryError):
+			frappe.get_doc({"doctype": "Rakoli Loan"}).insert(ignore_permissions=True)
+
+	def test_autoname_uses_rakoli_loan_series(self):
+		employee = create_employee()
+		loan = frappe.get_doc(
+			{
+				"doctype": "Rakoli Loan",
+				"employee": employee.name,
+				"loan_amount": 250000,
+			}
+		)
+		loan.insert(ignore_permissions=True)
+		self.assertTrue(loan.name.startswith("RAK-LOAN-"))
+
+	def test_rejects_status_outside_the_select_options(self):
+		employee = create_employee()
+		loan = frappe.get_doc(
+			{
+				"doctype": "Rakoli Loan",
+				"employee": employee.name,
+				"loan_amount": 250000,
+				"loan_status": "cancelled",
+			}
+		)
+		with self.assertRaises(frappe.ValidationError):
+			loan.insert(ignore_permissions=True)
